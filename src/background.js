1e9e3ab176507a176047f652c02bc16416b7c1d5
Douglas Brum <douglas.mateus2005@gmail.com>
2024-05-18 00:22:32 -0300

    Redirects before loading page

--- src/background.js
+++ src/background.js
@@ -1,40 +1,46 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == "complete" && tab.url) {
-        console.log(tab.url)
-        if (tab.url.includes("instagram.com/accounts/login/")) {
-            console.log("login page")
-    
-            chrome.tabs.sendMessage(tabId, {
-                action: "add_button"
-            });
-        }
+    if (tab.url) {
 
-        if (tab.url.endsWith("instagram.com/")) {
-            checkLoginStateAndExecute(
-                // If logged
-                () => {
-                    chrome.tabs.update(tabId, { url: "https://www.instagram.com/accounts/convert_to_professional_account/" });
-                },
-                // If not logged
-                () => {
-                    chrome.tabs.sendMessage(tabId, {
-                        action: "add_button"
-                    });
-                })
-        }
-
+        // Não espera a página carregar pra dar redirect
         if (tab.url.includes("instagram.com/accounts/onetap/")) {
-            console.log("Onetap page")
             chrome.tabs.update(tabId, { url: "https://www.instagram.com/accounts/convert_to_professional_account/" });
         }
 
-        if (tab.url.includes("instagram.com/accounts/suspended/")) {
-            console.log("Account suspended")
-            chrome.tabs.sendMessage(tabId, { action: "send_suspended_status" })
+        if (changeInfo.status == "complete") {
+            chrome.storage.local.get(["settings"])
+            .then(data => {
+                const { settings } = data;
+
+                if (tab.url.includes("instagram.com/accounts/login/")) {           
+                    chrome.tabs.sendMessage(tabId, {
+                        action: "add_button",
+                        settings
+                    });
+                }
+
+                if (tab.url.endsWith("instagram.com/")) {
+                    checkLoginStateAndExecute(
+                        // If logged
+                        () => {
+                            chrome.tabs.update(tabId, { url: "https://www.instagram.com/accounts/convert_to_professional_account/" });
+                        },
+                        // If not logged
+                        () => {
+                            chrome.tabs.sendMessage(tabId, {
+                                action: "add_button",
+                                settings
+                            });
+                        });
+                }
+
+                if (tab.url.includes("instagram.com/accounts/suspended/")) {
+                    console.log("Account suspended");
+                    chrome.tabs.sendMessage(tabId, { action: "send_suspended_status" });
+                }
+            });
         }
     }
-
-})
+});
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(sender, message.action)
@@ -42,17 +48,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     chrome.storage.local.get(["settings"])
     .then(data => {
         console.log("settings", data.settings)
-        if (message.action == "get_account") getAccountRequest(message, sender, sendResponse);
+        const { settings } = data;
+        if (message.action == "get_account") getAccountRequest(message, sender, sendResponse, settings);
             
-        if (message.action == "next_account") nextAccountRequest(message, sender, sendResponse);
+        if (message.action == "next_account") nextAccountRequest(message, sender, sendResponse, settings);
     
-        if (message.action == "update_account") updateAccountRequest(message, sender, sendResponse);
+        if (message.action == "update_account") updateAccountRequest(message, sender, sendResponse, settings);
     
-        if (message.action == "get_status") getCustomStatusRequest(message, sender, sendResponse);
+        if (message.action == "get_status") getCustomStatusRequest(message, sender, sendResponse, settings);
     
-        if (message.action == "check_server") checkServerRequest(message, sender, sendResponse);
+        if (message.action == "check_server") checkServerRequest(message, sender, sendResponse, settings);
     
-        if (message.action == "suspended_account") suspendedAccountRequest(message, sender, sendResponse);
+        if (message.action == "suspended_account") suspendedAccountRequest(message, sender, sendResponse. settings);
 
         if (message.action == "save_settings") chrome.storage.local.set({ settings: message.settings });
 
@@ -81,9 +88,9 @@ const checkLoginStateAndExecute = (isLoggedCallback, isNotLoggedCallback)  => {
     })
 }
 
-const getNextAccount = () => {
+const getNextAccount = (url) => {
     console.log("Getting next account")
-    return fetch('http://127.0.0.1:8080/account/next')
+    return fetch(`${url}/account/next`)
         .then(response => {
             if (response.status === 204) {
                 chrome.storage.local.remove(["token", "login", "password"], () => {});
@@ -101,29 +108,29 @@ const getNextAccount = () => {
         .catch(err => ({ success: false, error: err }))
 }
 
-const getAccountRequest = (message, sender, sendResponse) => {
+const getAccountRequest = (message, sender, sendResponse, settings) => {
     getCurrentAccount(currentAccount => {
         console.log(currentAccount)
         if (currentAccount.token) {
             sendResponse({ success: true, data: currentAccount });
             return;
         }
-        getNextAccount()
+        getNextAccount(settings.serverUrl)
             .then(response => sendResponse(response));
     })
 }
 
-const nextAccountRequest = (message, sender, sendResponse) => {
-    getNextAccount()
+const nextAccountRequest = (message, sender, sendResponse, settings) => {
+    getNextAccount(settings.serverUrl)
             .then(account => sendResponse({ success: true, data: account }))
             .catch(err => sendResponse({ success: false, error: err }));
 }
 
-const updateAccountRequest = (message, sender, sendResponse) => {
+const updateAccountRequest = (message, sender, sendResponse, settings) => {
     getCurrentAccount(currentAccount => {
         let url;
-        if (message.isSuspended) url = `http://127.0.0.1:8080/account/suspended?token=${currentAccount.token}&message=${message.statusMessage}`;
-        else url = `http://127.0.0.1:8080/account/update?token=${currentAccount.token}&status=${message.status}`;
+        if (message.isSuspended) url = `${settings.serverUrl}/account/suspended?token=${currentAccount.token}&message=${message.statusMessage}`;
+        else url = `${settings.serverUrl}/account/update?token=${currentAccount.token}&status=${message.status}`;
 
         fetch(url, { method: "PUT"})
             .then(response => response.json())
@@ -146,19 +153,19 @@ const updateAccountRequest = (message, sender, sendResponse) => {
                 .then(data => sendResponse(data));
                 // sendResponse(data);
             })
-            .catch(err => sendResponse({ success: false, error: `http://127.0.0.1:8080/account/update?token=${currentAccount.token}&status=${message.status}` }));
+            .catch(err => sendResponse({ success: false, error: err }));
     })
 }
 
-const checkServerRequest = (message, sender, sendResponse) => {
-    fetch("http://127.0.0.1:8080/info/health")
+const checkServerRequest = (message, sender, sendResponse, settings) => {
+    fetch(`${settings.serverUrl}/info/health`)
             .then(response => response.json())
             .then(data => sendResponse(data))
             .catch(err => sendResponse({ success: false }));
 }
 
-const getCustomStatusRequest = (message, sender, sendResponse) => {
-    fetch(`http://127.0.0.1:8080/status`)
+const getCustomStatusRequest = (message, sender, sendResponse, settings) => {
+    fetch(`${settings.serverUrl}/status`)
             .then(response => response.json())
             .then(data => sendResponse(data))
             .catch(err => sendResponse({ success: false, error: err }));    
